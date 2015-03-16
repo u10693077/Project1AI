c51fa9a90ca617d9c53452eec9ae8f8bf1103dab
Jandre' Coetzee <u10693077@tuks.co.za>
2015-03-16 21:14:55 +0200

    Updated backend

---
+++ BetaUI.py
@@ -0,0 +1,261 @@
+import Tkinter as tk
+from Tkinter import *
+import time
+from random import randint
+from backend import Game
+
+TITLE_FONT = ("Helvetica", 18, "bold")
+
+
+class CellWars(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+
+        container = tk.Frame(self)
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+
+        self.frames = {}
+        for F in (StartPage, PlayerVsPlayer, PlayerVsAI, AIvsAI):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+
+        self.show_frame(StartPage)
+
+    def show_frame(self, c):
+        '''Show a frame for the given class'''
+        frame = self.frames[c]
+        frame.tkraise()
+
+class StartPage(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Welcome To Cell Wars", font=TITLE_FONT)
+        label.pack(side="top", fill="x", pady=10)
+
+        button1 = tk.Button(self, text="Player vs. Player", width=20,
+                            command=lambda: controller.show_frame(PlayerVsPlayer))
+        button2 = tk.Button(self, text="Player vs. AI", width=20,
+                            command=lambda: controller.show_frame(PlayerVsAI))
+        button3 = tk.Button(self, text="AI vs. AI", width=20,
+                            command=lambda: controller.show_frame(AIvsAI))
+        button1.pack()
+        button2.pack()
+        button3.pack()
+
+
+
+class PlayerVsPlayer(tk.Frame):
+    def __init__(self, parent, controller):
+		tk.Frame.__init__(self, parent)
+		label = tk.Label(self, text="Player vs. Player", font=TITLE_FONT)
+		label.pack(side="top", fill="x", pady=10)
+		buttonBack = tk.Button(self, text="Back", width=20,
+						   command=lambda: controller.show_frame(StartPage))
+		buttonStart = tk.Button(self, text="Start", width=20, bg="red",
+						   command=lambda: self.start(gridSize, initialCell))
+
+		labelGridSize = tk.Label(self, text="Grid Size (even number >8):", font=12)
+
+		labelInitialCell = tk.Label(self, text="Initial Cells:", font=12)
+
+		gridSize = tk.Entry(self, bd=5, justify=CENTER, textvariable=StringVar())
+		gridSize.delete(0, END)
+		gridSize.insert(0,"12")
+
+
+
+		initialCell = tk.Entry(self, bd=5, justify=CENTER, textvariable=StringVar())
+		initialCell.delete(0, END)
+		initialCell.insert(0,"5")
+
+		labelGridSize.pack()
+		gridSize.pack()
+		labelInitialCell.pack()
+		initialCell.pack()
+		buttonStart.pack()
+		buttonBack.pack()
+
+    def start(self, gridSize, initialCell):
+        CreatBoard(gridSize.get().strip(), initialCell.get().strip())
+
+class PlayerVsAI(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="Player vs. AI", font=TITLE_FONT)
+        label.pack(side="top", fill="x", pady=10)
+        buttonBack = tk.Button(self, text="Back", width=20,
+                           command=lambda: controller.show_frame(StartPage))
+        buttonStart = tk.Button(self, text="Start", width=20, bg="red",
+                           command=lambda: controller.show_frame(StartPage))
+
+        labelGridSize = tk.Label(self, text="Grid Size (even number >8):", font=12)
+        gridSize = Entry(self, bd=5, justify=CENTER, textvariable=StringVar())
+        gridSize.delete(0, END)
+        gridSize.insert(0,"12")
+
+        labelInitialCell = tk.Label(self, text="Initial Cells:", font=12)
+        initialCell = tk.Entry(self, bd=5, justify=CENTER, textvariable=StringVar())
+        initialCell.delete(0, END)
+        initialCell.insert(0,"5")
+
+        labelPlyDepth= tk.Label(self, text="Ply Depth:", font=12)
+        plyDepth = Entry(self, bd=5, justify=CENTER, textvariable=StringVar())
+
+        aiType = IntVar()
+
+        minimax = Radiobutton(self, text="Minimax", variable=aiType, value=1)
+
+        minimaxAlpBe = Radiobutton(self, text="Minimax with alpha-beta pruning", variable=aiType, value=2)
+
+        labelGridSize.pack()
+        gridSize.pack()
+        labelInitialCell.pack()
+        initialCell.pack()
+        labelPlyDepth.pack()
+        plyDepth.pack()
+        minimax.pack(anchor = W)
+        minimaxAlpBe.pack(anchor = W)
+        buttonStart.pack()
+        buttonBack.pack()
+
+
+
+class AIvsAI(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        label = tk.Label(self, text="AI vs. AI", font=TITLE_FONT)
+        label.pack(side="top", fill="x", pady=10)
+        buttonBack = tk.Button(self, text="Back", width=20,
+                           command=lambda: controller.show_frame(StartPage))
+        buttonStart = tk.Button(self, text="Start", width=20, bg="red",
+                           command=lambda: controller.show_frame(StartPage))
+
+        labelGridSize = tk.Label(self, text="Grid Size (even number >8):", font=12)
+        gridSize = Entry(self, bd=5, justify=CENTER, textvariable=StringVar())
+        gridSize.delete(0, END)
+        gridSize.insert(0,"12")
+
+        labelInitialCell = tk.Label(self, text="Initial Cells:", font=12)
+        initialCell = tk.Entry(self, bd=5, justify=CENTER, textvariable=StringVar())
+        initialCell.delete(0, END)
+        initialCell.insert(0,"5")
+
+        labelPlyDepth= tk.Label(self, text="Ply Depth:", font=12)
+        plyDepth = Entry(self, bd=5, justify=CENTER, textvariable=StringVar())
+
+        aiType = IntVar()
+
+        minimax = Radiobutton(self, text="Minimax", variable=aiType, value=1)
+
+        minimaxAlpBe = Radiobutton(self, text="Minimax with alpha-beta pruning", variable=aiType, value=2)
+
+        labelGridSize.pack()
+        gridSize.pack()
+        labelInitialCell.pack()
+        initialCell.pack()
+        labelPlyDepth.pack()
+        plyDepth.pack()
+        minimax.pack(anchor = W)
+        minimaxAlpBe.pack(anchor = W)
+        buttonStart.pack()
+        buttonBack.pack()
+
+
+class CreatBoard():
+    pressed_count = 0
+    play_turn = 0
+    
+    def __init__(self, grSize, inCell):
+
+        self.N = int(grSize)
+        self.matrix = [[0 for x in range(self.N)] for x in range(self.N)]
+        self.all_buttons = []
+
+        self.game = Game(self.N,int(inCell))
+        print int(inCell)
+
+        self.board = Tk()
+        self.board.title('Cell Wars')
+        self.creatMatrix()
+        board= self.game.getBoard()
+
+    def run(self):
+        self.board.mainloop()
+
+
+    def creatMatrix(self):
+        for y, row in enumerate(self.matrix):
+            buttons_row = []
+            for x, element in enumerate(row):
+                boton = Button(self.board, width=6, height=2, command=lambda a=x,b=y: self.onButtonPressed(a,b))
+                boton.grid(row=y, column=x)
+                buttons_row.append( boton )
+            self.all_buttons.append( buttons_row )
+        self.draw()
+
+
+    def onButtonPressed(self, y, x):
+        print "pressed: x=%s y=%s" % (x, y)
+        CreatBoard.pressed_count += 1
+        
+        
+        if (CreatBoard.pressed_count == 1):
+            self.old_coord = (x,y)
+            self.all_buttons[x][y]['bg'] = '#%02x%02x%02x' % (0, 255, 0)
+        if (CreatBoard.pressed_count == 2):
+            if self.old_coord == (x,y):
+                CreatBoard.pressed_count = 0
+                self.old_coord = (0,0)
+                self.draw()
+            else:
+                CreatBoard.pressed_count = 0  
+                self.game.move(self.old_coord,(x,y),self.game.getPlayers(CreatBoard.play_turn))
+                if (CreatBoard.play_turn == 0):
+                    CreatBoard.play_turn = 1
+                else:
+                    CreatBoard.play_turn = 0
+                    
+                self.draw()
+
+        #print " "
+        #print "%d  %d" % (x,y)        
+        #print "Press_Count: %d" % (CreatBoard.pressed_count)
+        #print self.old_coord
+        #print " "
+        
+        # Light Red: '#%02x%02x%02x' % (255, 229, 204)
+        # ReD: '#%02x%02x%02x' % (255, 0, 0)
+        # Blue: '#%02x%02x%02x' % (0, 0, 255)
+        # Light Blue: '#%02x%02x%02x' % (204, 204, 255)
+
+    def draw(self):
+        board = self.game.getBoard()
+        for c_x in range(self.N):
+			tmp = ""
+			for c_y in range(self.N):
+				if "0*" == board[(c_x,c_y)]:
+					#print "LightRed"
+					self.all_buttons[c_x][c_y]['bg'] = '#%02x%02x%02x' % (255, 229, 204)
+				elif "1*" == board[(c_x,c_y)]:
+					#print "LightBlue"
+					self.all_buttons[c_x][c_y]['bg'] = '#%02x%02x%02x' % (204, 204, 255)
+				elif " " == board[(c_x,c_y)]:
+					#print "Clear"
+					self.all_buttons[c_x][c_y]['bg'] = '#%02x%02x%02x' % (216, 216, 216)
+				elif len(str(board[(c_x,c_y)])) == 1:
+					p_id = int(board[(c_x,c_y)])
+					#print p_id
+					self.all_buttons[c_x][c_y]['bg'] = '#%02x%02x%02x' % (255*abs(p_id-1), 0, p_id*255)
+				else:
+					#print "Green"
+					self.all_buttons[c_x][c_y]['bg'] = '#%02x%02x%02x' % (0, 255, 0)
+
+
+if __name__ == "__main__":
+    cw = CellWars()
+    cw.title("Cell Wars")
+    cw.geometry("300x350")
+    cw.mainloop()
---
+++ backend.py
@@ -0,0 +1,215 @@
+from random import randint
+
+class Game:
+	turn = 0
+	def __init__(self, N, number_default_cells,Debug=False):
+		self.N = N
+		self.players = {}
+
+		self.players[Player("Player 1")] = []
+		self.players[Player("Player 2")] = []
+
+		if not Debug:
+			for player in self.players:
+				id = player.getPlayerID()
+				print "Player:",id
+				for i in range(0,number_default_cells):
+					x = randint(id*(N/2)+id,(id+1)*(N/2)-1-1+id)
+					y = randint(0,N-1)
+					while self.hasCoord((x,y)):
+						#print "Duplicate Found"
+						x = randint(id*(N/2)+id,(id+1)*(N/2)-1-1+id)
+						y = randint(0,N-1)
+					self.move((-1,-1),(x,y),player)
+
+	def getPlayers(self,id=-1):
+		if id == -1:
+			return self.players
+		else:
+			for player in self.players:
+				if player.getPlayerID() == id:
+					return player
+
+	def move(self,coord_old,coord_new,player):
+		if coord_old == (-1,-1):
+			#print "Added new coord at", coord_new
+			group = self.findGroup(coord_new,player)
+			group.addCoord(coord_new)
+
+		else:
+			group = self.findGroup(coord_old,player)
+			
+			new_group = self.findGroup(coord_new,player)
+			new_group.addCoord(coord_new)
+
+                        group.removeCoord(coord_old)
+                        
+			for pair in self.isTouching(new_group):
+				coord = pair[0]
+				group = pair[1]
+				group.removeCoord(coord)
+				new_group.addCoord(coord)
+
+			print "Moved from", coord_old, "==>", coord_new
+
+	def isTouching(self,group):
+		span = group.getSpanMinCorners()
+		arr = []
+		for player in self.players:
+			for group in self.players[player]:
+				for coord in group.getCoords():
+					if coord in span:
+						arr.append((coord,group))
+		return arr
+	def hasCoord(self,coord):
+		for player in self.players:
+			for group in self.players[player]:
+				if group.contains(coord):
+					return True
+			return False
+
+	def findGroup(self,coord,player,all=False):
+		span = []
+		for x in range(max(0,coord[0]-2),min(self.N-1,coord[0]+2+1)):
+			for y in range(max(0,coord[1]-2),min(self.N-1,coord[1]+2+1)):
+				##print (x,y)
+				span.append((x,y))
+		if not all:
+		#for player in self.players:
+			for group in self.players[player]:
+				if group.intersects(span):
+					#print "Group Found"
+					return group
+			group = Group()
+			self.players[player].append(group)
+			return group
+		else:
+			for player in self.players:
+				for group in self.players[player]:
+					if group.intersects(span):
+						#print "Group Found"
+						return [group,player]
+
+
+
+	def getBoard(self):
+		print "\n\n"
+		board = {}
+		for x in range(0,self.N):
+			for y in range(0,self.N):
+				board[(x,y)] = ' '
+
+		for player in self.players:
+			for group in self.players[player]:
+				if len(group.getCoords()) > 0:
+					for coord in group.getSpan():
+						board[coord] = str(player.getPlayerID())+'*'
+
+		for player in self.players:
+			for group in self.players[player]:
+				for coord in group.getCoords():
+					board[coord] = player.getPlayerID()
+
+		for x in range(0,self.N):
+			tmp = ""
+			for y in range(0,self.N):
+				tmp = "%s|%3s|" % (tmp, str(board[(x,y)]))
+			print tmp
+
+		return board
+
+class Player:
+	'Common base class for all games'
+	player_count = 0
+
+	def __init__(self, name):
+		self.name = name
+		Player.player_count += 1
+		self.player_id = Player.player_count - 1
+		print "Player Joined: Name : ", self.name,  ", Player ID: ", self.player_id
+
+	def displayPlayer(self):
+		print self.name + "," +  str(self.player_id)
+
+	def getPlayerID(self):
+		return self.player_id
+
+class Group:
+	def __init__(self):
+		self.coords = []
+                print self.coords
+	def getCoords(self):
+		#print self.coords
+		return self.coords
+
+	def removeCoord(self,coord):
+		#print "Removing",coord
+                #print self.coords
+		self.coords.remove(coord)
+		#print self.coords
+
+	def addCoord(self,coord):
+		#print "Adding",coord
+		self.coords.append(coord)
+
+	def getCount(self):
+		print len(self.coords)
+
+	def contains(self,coord):
+		return (coord in self.coords)
+
+	def intersects(self,coords):
+		return len(set(self.getSpan())&set(coords))
+
+	def viewCoords(self):
+                print self.coords
+
+	def getSpan(self):
+		arr = []
+		x = []
+		y = []
+
+		for coord in self.coords:
+			x.append(coord[0])
+			y.append(coord[1])
+
+                print "Span"
+		print x
+		print y
+
+		for c_x in range(max(0,min(x)-1),max(x)+1+1):
+			tmp = ""
+			for c_y in range(max(0,min(y)-1),max(y)+1+1):
+				tmp += '(%s,%s);' % (c_x,c_y)
+				arr.append((c_x,c_y))
+			#print tmp
+		return arr
+
+	def getSpanMinCorners(self):
+		arr = []
+		x = []
+		y = []
+
+		for coord in self.coords:
+			x.append(coord[0])
+			y.append(coord[1])
+
+		for c_x in range(max(0,min(x)-2),max(x)+1+2):
+			tmp = ""
+			for c_y in range(max(0,min(y)-2),max(y)+1+2):
+				tmp += '(%s,%s);' % (c_x,c_y)
+				arr.append((c_x,c_y))
+			print tmp
+
+		arr.remove((max(x)+2,max(y)+2))
+		arr.remove((max(x)+2,max(0,min(y)-2)))
+		arr.remove((max(0,min(x)-2),max(y)+2))
+		arr.remove((max(0,min(x)-2),max(0,min(y)-2)))
+
+		return arr
+
+	def inSpan(self,coord):
+		return (coord in self.getSpan())
+
+	def overlap(self,coord):
+		return (coord in self.coords)
